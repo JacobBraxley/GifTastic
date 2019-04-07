b56980184432dbd9fc1646e68c7693ea8c0cccea
JacobBraxley <jacob@roboticsys.com>
2019-04-07 13:10:50 -0400

    Working

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -5,76 +5,74 @@
 //An area to add new buttons.
 const interestArray = ["Bugs Bunny", "Tweety", "Tasmanian Devil", "Daffy Duck", "Porky Pig", "Marvin the Martian", "Elmer Fudd", "Sylvester", "Pepe Le Pew"];
 
-function addNewButton() {
-    $("#newButton").removeClass("pulse"); // Users don't need a pulse after its been used.
-    const userInput = $("#newInput").val().trim();
-    interestArray.push(userInput);
+function addNewButton(userInput = $("#newInput").val().trim(), add = true) {
+    //const ;
+    if (add) {
+        interestArray.push(userInput);
+    }
 
-    const btnDom = $(`<button class='giphyFetcher' data-interest='${userInput}'>${userInput}</button>`);
-    $("#searchButtons").append(btnDom);
+    // const btnDom = $(`<button class='giphyFetcher' data-interest='${userInput}'>${userInput}</button>`);
+    const btnDom = $(`<button class="btn waves-effect deep-purple lighten-3 giphyFetcher" data-interest='${userInput}'><i class="material-icons left">gif</i>${userInput}</button>`);
+    btnDom.insertBefore($("#newButton"));
 }
 
 function displayGiphyResponse(response) {
-    $("#displayArea").clear();
-
-    response.data.array.forEach(element => {
-        let giphyDom = `${element}`;//TODO
+    $("#displayArea").empty();
+    response.data.forEach(element => {
+        let giphyDom = `<img src="${element.images.fixed_width_still.url}" 
+                        data-still="${element.images.fixed_width_still.url}" 
+                        data-animate="${element.images.fixed_width.url}" 
+                        data-state="still" 
+                        class="gif">`;
 
         $("#displayArea").append(giphyDom);
     });
 }
 
 function giphySearch(source) {
-    const searchTerm = $(this).attr("data-interest");
+    let searchTerm = $(this).attr("data-interest");
+    searchTerm = searchTerm.replace(/ /g, "+"); //Replace any spaces with pluses.
 
-    //svKQhrA5X2P0e8BNptFNyEWf87jROpZl
-    const query = ``;//TODO
-
-    //Use Query.
-    //Call displayGiphyResponse(response)
-
-    // var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC";
-
-    // $.ajax({
-    //   url: queryURL,
-    //   method: "GET"
-    // }).then(function(response) {
-    //   console.log(response);
-    // });
+    $.ajax({
+        url: `https://api.giphy.com/v1/gifs/search?q=${searchTerm}&api_key=svKQhrA5X2P0e8BNptFNyEWf87jROpZl&limit=10`,
+        method: "GET"
+    }).then(function (response) {
+        //console.log(response)
+        displayGiphyResponse(response)
+    });
 }
 
 function startOrStopGiphy(item) {
-    //Figure out how to start/stop giphys. $(item).
+    const state = $(item).attr("data-state");
+
+    //Switch the Src and update the state for future clicks.
+    if (state === "still") {
+        $(item).attr("src", $(item).attr("data-animate"));
+        $(item).attr("data-state", "animate");
+    } else {
+        $(item).attr("src", $(item).attr("data-still"));
+        $(item).attr("data-state", "still");
+    }
 }
 
-$("#newButton").on("click", addNewButton);
-$(".giphyFetcher").on("click", giphySearch);
-$(".giphyItem").on("click", startOrStopGiphy);
+function stopPulse() {
+    $("#newButton").removeClass("pulse"); // Users don't need a pulse after its been used.
+}
 
-$(document).ready(function(){
+interestArray.forEach(element => {
+    addNewButton(element, false);
+})
+
+$("#newButton").on("click", stopPulse);
+$("#submitInterest").on("click", function() {
+    const input = $("#newInput").val().trim();
+    addNewButton(input);
+});
+$(document).on("click", '.giphyFetcher', giphySearch);
+$(document).on('click', '.gif', function() {
+    startOrStopGiphy(this);
+});
+
+$(document).ready(function () {
     $('.modal').modal();
-  });
-
-
-//   <body>
-//   <img src="https://media1.giphy.com/media/3o85xkQpyMlnBkpB9C/200_s.gif" data-still="https://media1.giphy.com/media/3o85xkQpyMlnBkpB9C/200_s.gif" data-animate="https://media1.giphy.com/media/3o85xkQpyMlnBkpB9C/200.gif" data-state="still" class="gif">
-//   <img src="https://media2.giphy.com/media/8rFQp4kHXJ0gU/200_s.gif" data-still="https://media2.giphy.com/media/8rFQp4kHXJ0gU/200_s.gif" data-animate="https://media2.giphy.com/media/8rFQp4kHXJ0gU/200.gif" data-state="still" class="gif">
-//   <img src="https://media3.giphy.com/media/W6LbnBigDe4ZG/200_s.gif" data-still="https://media3.giphy.com/media/W6LbnBigDe4ZG/200_s.gif" data-animate="https://media3.giphy.com/media/W6LbnBigDe4ZG/200.gif" data-state="still" class="gif">
-//   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-//   <script type="text/javascript">
-//     $(".gif").on("click", function() {
-//       // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
-//       var state = $(this).attr("data-state");
-//       // If the clicked image's state is still, update its src attribute to what its data-animate value is.
-//       // Then, set the image's data-state to animate
-//       // Else set src to the data-still value
-//       if (state === "still") {
-//         $(this).attr("src", $(this).attr("data-animate"));
-//         $(this).attr("data-state", "animate");
-//       } else {
-//         $(this).attr("src", $(this).attr("data-still"));
-//         $(this).attr("data-state", "still");
-//       }
-//     });
-//   </script>
-// </body>
+});
